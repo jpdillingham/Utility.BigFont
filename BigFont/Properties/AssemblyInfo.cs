0437db2d2ff3ac62e248d1b873857e183ded909c
JP Dillingham <jpdillingham@gmail.com>
2016-08-14 16:59:57 -0500

    Updated assembly name.

--- BigFont/Properties/AssemblyInfo.cs
+++ BigFont/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BigFont")]
+[assembly: AssemblyTitle("Utility.BigFont")]
 [assembly: AssemblyDescription("A C# Class Library for transforming strings into large, stylized characters.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BigFont")]
+[assembly: AssemblyProduct("Utility.BigFont")]
 [assembly: AssemblyCopyright("Copyright (c) 2016 JP Dillingham (jp@dillingham.ws)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.5")]
-[assembly: AssemblyFileVersion("2.0.0.5")]
+[assembly: AssemblyVersion("2.0.0.6")]
+[assembly: AssemblyFileVersion("2.0.0.6")]
