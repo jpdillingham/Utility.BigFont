6ac5ed7f1eb117be510e94f8c387056cf965c615
JP Dillingham <jpdillingham@gmail.com>
2017-03-26 18:02:47 -0500

    Updated assemblyinfo

--- Utility.BigFont/Properties/AssemblyInfo.cs
+++ Utility.BigFont/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following set of attributes. Change these attribute values to
-// modify the information associated with an assembly.
 [assembly: AssemblyTitle("Utility.BigFont")]
 [assembly: AssemblyDescription("A C# Class Library for transforming strings into large, stylized characters.")]
 [assembly: AssemblyConfiguration("")]
@@ -12,18 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (c) 2016 JP Dillingham (jp@dillingham.ws)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible to COM components. If you need to access a type in this
-// assembly from COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("14f214e3-9f68-4be8-b81c-60daab4da6df")]
-
-// Version information for an assembly consists of the following four values:
-//
-// Major Version Minor Version Build Number Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below: [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.1.1.10")]
 [assembly: AssemblyFileVersion("2.1.1.10")]
